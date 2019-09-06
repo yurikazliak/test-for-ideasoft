3bbcd56e572e5fa1d63a5646b3eca787d9170294
Yuri Kazliak <47756849+yurikazliak@users.noreply.github.com>
2019-09-06 08:59:30 +0300

    refactor: rewrite App as functional component

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import logo from '../logo.svg';
 
 import { fetchArr } from '../store/actions';
 
-class App extends Component {
-
-  render() {
-
-    return (
-      <div className='App'>
-        <button className='Button'
-        onClick={() => this.props.dispatch(fetchArr())}
-          >Click on me!
+const App = (props) => {
+  const { array, loading, error, dispatch } = props;
+  return (
+    <div className='App'>
+      <button className='Button'
+        onClick={() => dispatch(fetchArr())}
+      >Click on me!
             </button>
-        {this.props.loading
-          ? <img src={logo} className="App-logo" alt="logo" />
-          : this.props.error
-            ? <p className='Load-error'>Error, something went wrong</p>
-            : <p className='Load-success'>{this.props.array.join(' ')}</p>}
-      </div>
-    )
-  }
+      {loading
+        ? <img src={logo} className="App-logo" alt="logo" />
+        : error
+          ? <p className='Load-error'>Error, something went wrong</p>
+          : <p className='Load-success'>{array.join(' ')}</p>}
+    </div>
+  )
 }
 
 export default App;
